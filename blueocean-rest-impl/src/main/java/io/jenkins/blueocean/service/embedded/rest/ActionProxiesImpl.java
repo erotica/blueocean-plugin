86f82928556049a80a54c540819281775779d07f
James Dumay <jdumay@cloudbees.com>
2017-01-01 13:02:07 +1100

    Add a banned_actions.txt to remove poorly performing actions (such as FlowGraphAction) from REST responses.

--- blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/rest/ActionProxiesImpl.java
+++ blueocean-rest-impl/src/main/java/io/jenkins/blueocean/service/embedded/rest/ActionProxiesImpl.java
@@ -1,5 +1,13 @@
 package io.jenkins.blueocean.service.embedded.rest;
 
+import com.google.common.base.Charsets;
+import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
+import com.google.common.io.Resources;
 import hudson.model.Action;
 import io.jenkins.blueocean.rest.Reachable;
 import io.jenkins.blueocean.rest.hal.Link;
@@ -8,11 +16,17 @@ import org.kohsuke.stapler.export.ExportedBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import javax.annotation.Nullable;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 /**
  * @author Vivek Pandey
  */
 public class ActionProxiesImpl extends BlueActionProxy {
-
     private final Action action;
     private final Reachable parent;
     private static final Logger logger = LoggerFactory.getLogger(ActionProxiesImpl.class);
@@ -54,4 +68,36 @@ public class ActionProxiesImpl extends BlueActionProxy {
         }
         return null;
     }
+
+    public static Collection<BlueActionProxy> getActionProxies(List<? extends Action> actions, Reachable parent){
+        List<BlueActionProxy> actionProxies = new ArrayList<>();
+        for(Action action : Iterables.filter(actions, NOT_BANNED)){
+            if(action == null){
+                continue;
+            }
+            actionProxies.add(new ActionProxiesImpl(action, parent));
+        }
+        return actionProxies;
+
+    }
+
+    private static final Supplier<ImmutableList<String>> BANNED_ACTIONS = Suppliers.memoize(new Supplier<ImmutableList<String>>() {
+        @Override
+        public ImmutableList<String> get() {
+            URL url = ActionProxiesImpl.class.getClassLoader().getResource("io.jenkins.blueocean.service.embedded/banned_actions.txt");
+            assert url != null;
+            try {
+                return ImmutableList.copyOf(Resources.readLines(url, Charsets.UTF_8));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    });
+
+    private static final Predicate<Action> NOT_BANNED = new Predicate<Action>() {
+        @Override
+        public boolean apply(Action action) {
+            return !BANNED_ACTIONS.get().contains(action.getClass().getName());
+        }
+    };
 }
